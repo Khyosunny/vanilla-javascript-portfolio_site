5beea506826fe5645bff9621ea37e88d91cb4231
Khyosunny <71235165+Khyosunny@users.noreply.github.com>
2020-09-25 00:36:00 +0900

    JS: Home contact button

--- main.js
+++ main.js
@@ -14,12 +14,69 @@ document.addEventListener("scroll", () => {
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener("click", (event) => {
-  const target = event.target;
+  const target = event.target; // 이벤트가 발생한 Html 요소(클릭한 li를 찾는 것)
   const link = target.dataset.link;
   if (link == null) {
     return;
+  } else {
+    const scrollTo = document.querySelector(link);
+    const top = scrollTo.offsetTop - navbarHeight;
+    // scrollTo.scrollIntoView({ behavior: "smooth" });
+    window.scrollTo({
+      top: top,
+      behavior: "smooth",
+    });
   }
-  console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
+
+const home = document.querySelector("#home");
+const about = document.querySelector("#about");
+const skills = document.querySelector("#skills");
+const work = document.querySelector("#work");
+const contact = document.querySelector("#contact");
+
+// const homeTop = home.getBoundingClientRect().top; // 0
+// const aboutTop = about.getBoundingClientRect().top; // 732
+// const skillsTop = skills.getBoundingClientRect().top; // 1647
+// const workTop = work.getBoundingClientRect().top; // 2401
+// const contactTop = contact.getBoundingClientRect().top; // 3802
+
+const topList = [0, 732, 1647, 2401, 3802];
+
+const navHome = document.querySelector("#navhome");
+const navAbout = document.querySelector("#navabout");
+const navSkills = document.querySelector("#navskills");
+const navWork = document.querySelector("#navwork");
+const navContact = document.querySelector("#navcontact");
+
+window.addEventListener("scroll", () => {
+  const winTop = window.document.documentElement.scrollTop;
+  let activeBtn = document.querySelector(".navbar__menu__item.active");
+  const classRemove = () => {
+    activeBtn.classList.remove("active");
+  };
+  console.log(winTop);
+  if (winTop >= 0 && winTop <= 627) {
+    classRemove();
+    navHome.classList.add("active");
+  } else if (winTop >= 628 && winTop <= 1494) {
+    classRemove();
+    navAbout.classList.add("active");
+  } else if (winTop >= 1495 && winTop <= 2254) {
+    classRemove();
+    navSkills.classList.add("active");
+  } else if (winTop >= 2255 && winTop <= 2680) {
+    classRemove();
+    navWork.classList.add("active");
+  } else {
+    classRemove();
+    navContact.classList.add("active");
+  }
+});
+
+// Contact Me 버튼 클릭시 이동
+
+const contactMeBtn = document.querySelector(".home__contact");
+contactMeBtn.addEventListener("click", () => {
+  contact.scrollIntoView({ behavior: "smooth" });
 });
